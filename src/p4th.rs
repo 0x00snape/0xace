a1819db89c6d074f5c431ff7f67fee7f06343e53
Severus Snape <144463175+0x00snape@users.noreply.github.com>
2024-02-20 16:11:25 +0000

    Update p4th.rs

--- src/p4th.rs
+++ src/p4th.rs
@@ -1,4 +1,4 @@
-use std::{fmt::format, fs::{self, File}, io::{self,prelude::*}, path::Path, process::exit};
+use std::{env::{self, current_dir}, fmt::format, fs::{self, File}, io::{self,prelude::*}, path::Path, process::exit};
 
 
 pub fn get_path() -> String {
@@ -13,41 +13,41 @@ pub fn get_path() -> String {
     _path
 }
 
-pub fn check_path(path: String) -> String {
 
-    let path = Path::new(&path);
-        
-        // Checking directory or file
-        if path.is_dir() == true { 
 
-            let path = std::fs::read_dir(&path).unwrap();
-            let mut _path = "".to_string();   
+pub fn recursive(path: String) -> String {
+
+    let path = Path::new(&path);  
             
-            for files in path {
+    //Directory exists or not 
+    match fs::metadata(path) {
 
-                    let mut files = files.unwrap().path().display().to_string();
-                    let files = Path::new(&files);
-                   
-                    _path.push_str(files.display().to_string().as_str());
-                    _path.push_str("\n");
-                    
-                }
-             _path
-        
-        } else { 
-            
-            // file/Directory exists or not 
-            match fs::metadata(path) { 
-                Ok(_) => { 
-                           path.display().to_string()
-                         },
-                Err(_) => {
-                            println!("[*] File/Directory doesn't exist !!!\n[*] Ensure that you have chosen the correct path...");
-                            exit(1)
-                        },
-            }
-        }
+        Ok(_) => { 
+                    if path.is_dir() == true { 
+
+                        let path = std::fs::read_dir(&path).unwrap();
+                        let mut _path = "".to_string();   
+                        
+                            for files in path {
+
+                                    let mut files = files.unwrap().path().display().to_string();
+                                    let files = Path::new(&files);
+                               
+                                    _path.push_str(files.display().to_string().as_str());
+                                    _path.push_str("\n");
+                                
+                            }
+                         _path
+                    } else { 
+                                path.display().to_string()
+                    }
+                 },
+
+        Err(_) => {
+                    println!("[*] File/Path {} doesn't exist !!!\nExample:\nPath:> /src/test.txt\nPath:> /src/test/", path.display().to_string());
+                    exit(1)
+                },
+    }
 
 }
-
 
